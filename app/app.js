f0e9bb284c8fb72e7d2ae4693d9439d5956cfb28
daniestrijdom <daniel.strijdom@addynamo.com>
2018-08-04 17:45:25 +0200

    add basic route for home page

--- app/app.js
+++ app/app.js
@@ -1,7 +1,17 @@
 const app = require("express")();
+const http = require("http").Server(app);
+
+// health check endpoint
+app.use("/healthCheck", function healthCheck(req, res) {
+  res.sendStatus(200);
+});
+
+const routes = require("./routes");
+
+app.use(routes);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log("info", `Serving port port ${port}`);
 });
---
+++ app/pages/home.js
@@ -0,0 +1,6 @@
+const home = (req, res) => {
+  res.status(200);
+  res.send("HOME PAGE");
+};
+
+module.exports = home;
