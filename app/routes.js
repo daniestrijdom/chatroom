2787171f1e6699f005f1e96eeb386982ba81ae62
daniestrijdom <daniel.strijdom@addynamo.com>
2018-08-05 12:55:19 +0200

    update a mock enpoint for rooms

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const middleware = require("./middleware");
 const router = require("express").Router();
 
 router.get("/", middleware.home);
+router.get("/rooms/:id", (req, res) => {
+  res.sendStatus(200).json({ all: "good" });
+});
 
 module.exports = router;
