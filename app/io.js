b59d92c339bf54ad9279eae20708582f719f93d1
daniestrijdom <daniel.strijdom@addynamo.com>
2018-08-09 18:44:36 +0200

    add basic conversation functionality and clean up chatroom css

--- app/io.js
+++ app/io.js
@@ -3,6 +3,7 @@ const logger = require("logops");
 const connect = httpModule => {
   const io = require("socket.io")(httpModule);
   const connections = [];
+  const users = [];
 
   io.on("connection", function(socket) {
     connections.push(socket);
@@ -10,14 +11,28 @@ const connect = httpModule => {
 
     // disconnect
     socket.on("disconnect", function() {
+      users.splice(users.indexOf(socket.username), 1);
+      updateUsernames();
       connections.splice(connections.indexOf(socket), 1);
       logger.debug(`user disconnected, connections: ${connections.length}`);
     });
 
     // send message
     socket.on("send message", function(data) {
-      io.sockets.emit("new message", { msg: data });
+      io.sockets.emit("new message", { msg: data, user: socket.username });
     });
+
+    // new user
+    socket.on("new user", function(data, callback) {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsernames();
+    });
+
+    const updateUsernames = () => {
+      io.sockets.emit("get users", users);
+    };
   });
 };
 
