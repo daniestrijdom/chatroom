6d6d913b8e4dcad5f178a809d70ca5cc7a1b88fc
daniestrijdom <daniel.strijdom@addynamo.com>
2018-08-11 04:51:26 +0200

    move html scripts to own file

---
+++ app/public/css/room/script.js
@@ -0,0 +1,57 @@
+$(function() {
+  var socket = io.connect();
+
+  var $messageForm = $("#messageForm");
+  var $message = $("#message");
+  var $chat = $("#chat");
+  var $messageArea = $("#messageArea");
+  var $userFormArea = $("#userFormArea");
+  var $userForm = $("#userForm");
+  var $users = $("#users");
+  var $username = $("#username");
+
+  $messageForm.submit(function(e) {
+    e.preventDefault();
+    socket.emit("send message", $message.val());
+    $message.val("");
+  });
+
+  socket.on("new message", function(data) {
+    $chat
+      .append(
+        '<div class="message"><strong>' +
+          data.user +
+          ": </strong>" +
+          data.msg +
+          "</div><br>"
+      )
+      .animate({ scrollTop: $chat.prop("scrollHeight") }, 1);
+  });
+
+  $userForm.submit(function(e) {
+    e.preventDefault();
+    socket.emit("new user", $username.val(), function(data) {
+      if (data) {
+        $userFormArea.hide();
+        $messageArea.css("visibility", "visible");
+      }
+    });
+    $username.val(" ");
+  });
+
+  socket.on("get users", function(data) {
+    var html = "";
+
+    for (var i = 0; i < data.length; i++) {
+      html += "<li>  " + data[i] + "</li>";
+    }
+    $users.html(html);
+  });
+
+  $("textarea").keypress(function(e) {
+    if (e.which == 13) {
+      $("#messageForm").submit();
+      return false;
+    }
+  });
+});
